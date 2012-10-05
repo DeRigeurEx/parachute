f2c3eb8a979851d982ac2bc1bd23fc28b695d839
Michael Sheppard <pbrane0@gmail.com>
2012-10-05 11:19:21 -0400

    updated scripts

--- mkjar.sh
+++ mkjar.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-echo "== Creating jar =="
+echo "== Creating jars =="
 
 DATE=`date +%Y%m%d`
 
 MCMODDATA="
 [\n
 {\n
-  \"modid\": \"ParachuteMod\",\n
-  \"name\": \"Parachute Mod\",\n
-  \"description\": \"Adds a Parachute to the game. Jump off cliffs! Base Jumping!! Soar to new heights!\",\n
+  \"modid\": \"ReptileMod\",\n
+  \"name\": \"Reptile Mod\",\n
+  \"description\": \"Reptile mod adds monitor lizards, turtles, iguanas, chameleons, and crocodiles. Komodo Dragons! Man-eating Crocodiles! And cute little turtles.\",\n
   \"version\": \"$DATE\",\n
   \"mcversion\": \"1.3.2\",\n
   \"url\": \"http://www.minecraftforum.net/topic/585469-132modloader-crackedeggs-mods-reptiles-parachute-updated-08272012/\",\n
-  \"updateUrl\": \"http://www.minecraftforum.net/topic/585469-132modloader-crackedeggs-mods-reptiles-parachute-updated-08272012/\",\n
+  \"updateUrl\": \"\",\n
   \"authors\": [\n
     \"crackedEgg\"\n
   ],\n
   \"credits\": \"Authored by crackedEgg\",\n
-  \"logoFile\": \"/textures/chuteLogo.png\",\n
+  \"logoFile\": \"/logo/reptileLogo.png\",\n
   \"screenshots\": [],\n
   \"parent\": \"\",\n
   \"dependencies\": []\n
@@ -26,28 +26,26 @@ MCMODDATA="
 ]"
 
 echo "> copying files"
-#MODINFO="../../mcmod.info"
 
 REOBF="reobf/minecraft/"
 cd $REOBF
 
-PDIR="$HOME/projects/parachute-src-1.3.2"
+RDIR="$HOME/projects/reptiles-src-1.3.2"
 
-rm -f $PDIR/parachute/common/*.class
-rm -f $PDIR/parachute/client/*.class
+rm -f $RDIR/reptiles/common/*.class
+rm -f $RDIR/reptiles/client/*.class
 
-cp -R parachute/ $PDIR
-echo -e $MCMODDATA > $PDIR/mcmod.info
+cp -R reptiles/ $RDIR
+echo -e $MCMODDATA > $RDIR/mcmod.info
 
 echo "> making mod jar file"
 
-cd $PDIR
+cd $RDIR
 
-JAR="parachute-1.3.2-fml-251.jar"
+JAR="reptiles-1.3.2-fml-298.jar"
+
 rm -f $JAR
-
-jar -cf $JAR parachute/ mcmod.info textures/
-#mv $JAR ../
+jar -cf $JAR reptiles/  mcmod.info mob/ sound/ logo/
 
 echo " - Mod build complete"
 
--- place-jars.sh
+++ place-jars.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-MCCLIENTDIR="$HOME/minecraft-1.3.2-fml-251/mods"
-MCSERVERDIR="$HOME/mc-server-fml-251/mods"
-JARDIR="$HOME/projects/parachute-src-1.3.2"
-JAR="parachute-1.3.2-fml-251.jar"
+MCCLIENTDIR="$HOME/minecraft-1.3.2-fml-298/mods"
+MCSERVERDIR="$HOME/mc-server-fml-298/mods"
+JARDIR="$HOME/projects/reptiles-src-1.3.2"
+JAR="reptiles-1.3.2-fml-298.jar"
 
 cp -f "$JARDIR/$JAR" $MCCLIENTDIR
 cp -f "$JARDIR/$JAR" $MCSERVERDIR
