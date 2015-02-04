55fb028b7951aaa1ce5d8c1629ae1e3da78ec97d
m1k3s <pbrane0@gmail.com>
2015-02-04 13:14:53 -0500

    moved KeyPressTick class to client directory.

---
+++ parachute/client/KeyPressTick.java
@@ -0,0 +1,54 @@
+//  
+//  =====GPL=============================================================
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; version 2 dated June, 1991.
+// 
+//  This program is distributed in the hope that it will be useful, 
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program;  if not, write to the Free Software
+//  Foundation, Inc., 675 Mass Ave., Cambridge, MA 02139, USA.
+//  =====================================================================
+//
+//
+// Copyright 2011-2015 Michael Sheppard (crackedEgg)
+//
+package com.parachute.client;
+
+import com.parachute.common.KeyPressMessage;
+import com.parachute.common.PacketHandler;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.minecraft.entity.player.EntityPlayer;
+import org.lwjgl.input.Keyboard;
+
+// intercept the ascend key to make the parachute go up
+// the ascend key defaults to the space bar or jump key.
+public class KeyPressTick {
+
+	private final int ascendKey;
+	
+	public KeyPressTick(int key)
+	{
+		ascendKey = key;
+	}
+
+	@SubscribeEvent
+	public void onTick(TickEvent.PlayerTickEvent event)
+	{
+		if (event.phase.equals(TickEvent.Phase.START)) {
+			if (Keyboard.getEventKey() == ascendKey) { // only send if it's the ascend key
+				EntityPlayer player = event.player;
+				if (player != null) {
+					boolean keyPressed = Keyboard.isKeyDown(ascendKey);
+					PacketHandler.network.sendToServer(new KeyPressMessage(keyPressed));
+				}
+			}
+		}
+	}
+
+}
